d1f1c1f4a7b5db9df14607ceb2d86a8c3ac48b20
akaletr <dmitriy.polyakov@onlinesup.com>
2024-05-28 12:42:06 -0300

    initial commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	server := http.Server{
+		Addr: ":8888",
+	}
+
+	server.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("hello world"))
+	})
+
+	log.Fatal(server.ListenAndServe())
+}
